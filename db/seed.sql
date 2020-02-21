c80c548117ce80bc98b5990afd40e5a00c6e0301
Anders Lofgran <anders.lofgran@gmail.com>
2020-02-21 10:23:24 -0700

    setup complete

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO users
+(user_email, user_password)
+VALUES
+('helo@sim.com', 'helo'),
+('dev@mountain.com', 'dev');
---
+++ server/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express')
+const session = require('express-session')
+const massive = require('massive')
+const authCtrl = require('./authCtrl')
+const crudCtrl = require('./crudCtrl')
+
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
+
+const app = express()
+
+app.use(express.json())
+
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    rejectUnauthorized: false,
+    cookie: {maxAge: 1000 * 60 * 60 * 24},
+    secret: SESSION_SECRET
+  })
+)
+
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: {rejectUnauthorized: false}
+}).then(db => {
+  app.set('db', db)
+  app.listen(SERVER_PORT, () => console.log(`Listening on port ${SERVER_PORT}`))
+  console.log('DB connected')
+})
+
+
